7e41d2fa2846354ea9a6903ad56385b31df9ad2d
liis lehemets <liis.lehemets@tktk.ee>
2024-12-14 11:15:43 +0200

    add week5 to code

---
+++ OROperators/Program.cs
@@ -0,0 +1,43 @@
+﻿
+// math>= 90 OR Chemistry >=90 OR Biology>= 90
+// kui 1 täidetud, siis saab sisse ülikooli
+// math && chemistry ||math && biology || chemistry && biology - 
+
+//int math, chemistry;
+
+//Console.WriteLine("Enter your math result:");
+//math = Int32.Parse(Console.ReadLine());
+
+//Console.WriteLine("Enter your Chemistry result: ");
+//chemistry = Int32.Parse(Console.ReadLine());
+
+//if (math >= 90 || chemistry >= 90)
+{
+   // Console.WriteLine("Congratulations! You got accepted!");
+}
+//else
+{
+    //Console.WriteLine("Your application cannot be approved.");
+}
+
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry result: ");
+chemistry = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Biology result: ");
+biology = Int32.Parse(Console.ReadLine());
+
+
+if ((math >= 90 && chemistry >=90) ||(math >= 90 && biology >= 90) || (chemistry >= 90 && biology >= 90))
+{
+    Console.WriteLine("Congratulations! You got accepted!");
+}
+else
+{
+    Console.WriteLine("Your application cannot be approved.");
+}
---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,13 @@
+﻿
+// AND -&&
+//sisseastumistesti tulemused kõrgem või võrdne kui 90%
+
+//true && -->true mata ja bio peavad olema mõlemad tõesed, siis on tulemuseks tõene.
+// 1 on true && teine ei ole täidetud --> siis ei saa vastu võtta
+//false && true --> false.
+// false && false --> false kui mõlemad tingimused ei ole täidetud
+
+// OR- || (pipes)- pehmem tingimus
+// nt true ||true--> true
+//true|| false --> true - kuna 1 või teine tingimus oli sisendiks ja 1 on täidetud, seega sobib
+
